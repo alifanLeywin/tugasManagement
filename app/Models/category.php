ee9528b24bf1a508ba51eb78b304ade24fc6f2a2
Tsabitah-DJ <dhimasjulianttsabitah@gmail.com>
2025-06-03 09:16:02 +0700

    membuat Migration Dan Model

--- app/Models/category.php
+++ app/Models/category.php
@@ -9,6 +9,6 @@ class category extends Model
     //
     protected $fillable = [
         'name',
-        'is_expensive', 
+        'category', 
     ];
 }
--- database/migrations/2025_05_22_043218_create_categories_table.php
+++ database/migrations/2025_05_22_043218_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_expensive')->default(true);
+            $table->enum('Categories', ['expense','income'])->default('expense');
             $table->timestamps();
             $table->SoftDeletes();
         });
